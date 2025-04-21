8849c6e5ce43f778096267bfcc917c64be7e1800
Abhishek00013 <abhishekpatwal0013@gmail.com>
2025-04-21 22:44:41 +0530

    add basic rest api using express

---
+++ peer/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import config from "./config/appConfig.js";
+import heartbeatRoutes from "./routes/heartbeatRoutes.js";
+
+// Express configs
+const app = express();
+app.use(express.json());
+
+// API for Master
+app.use("/api/heartbeat", heartbeatRoutes);
+
+// Initializing Functions
+
+app.listen(config.node.port, () => {
+  console.log(`[${config.node.id}] Running on port ${config.node.port}`);
+});
---
+++ peer/routes/heartbeatRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+// GET /api/heartbeat
+router.get("/");
+
+export default router;
