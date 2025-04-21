b41a2fd1fec8e679d1fb3a208508ee40437848e4
Navalpreet Kaur <143615518+Naval284@users.noreply.github.com>
2025-04-21 23:18:32 +0530

    Update peerService.js

--- master/services/peerService.js
+++ master/services/peerService.js
@@ -4,7 +4,6 @@ import config from '../config/appConfig.js';
 export let peers = [];
 
 export const loadPeersOnRestart = async () => {
-  // Load list of already existing peers from master/persistent/peer-registry.json into the above variable called peers.
   try {
 
     const data = await fs.readFileSync(config.paths.peerRegistry, 'utf-8');
@@ -26,7 +25,5 @@ export const loadPeersOnRestart = async () => {
 };
 
 export const registerPeer = async (peer) => {
-// Develop Logic for getting the peer data from the argument passed in this function
-// and then saving that data into the global variable called peers above, and also saving the same info to
-// the file master/persistent/peer-registry.json
+
 };
