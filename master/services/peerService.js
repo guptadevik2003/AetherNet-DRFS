5fe47fbd157878aa6c4b18dfee34d0f2372876ca
Navalpreet Kaur <143615518+Naval284@users.noreply.github.com>
2025-04-21 23:15:01 +0530

    Update peerService.js

--- master/services/peerService.js
+++ master/services/peerService.js
@@ -5,6 +5,24 @@ export let peers = [];
 
 export const loadPeersOnRestart = async () => {
   // Load list of already existing peers from master/persistent/peer-registry.json into the above variable called peers.
+  try {
+
+    const data = await fs.readFileSync(config.paths.peerRegistry, 'utf-8');
+    const parsed = JSON.parse(data);
+
+    if(Array.isArray(parsed.peers)) {
+
+      peers = parsed.peers;
+
+    } else {
+      console.log(`persistent/peer-registry.json doesn't contain 'peers' array. Starting fresh.`);
+      peers = [];
+    }
+
+  } catch(err) {
+    console.log(`Failed to load persistent/peer-registry.json. Starting fresh.\nError:`, err.message);
+    peers = [];
+  }
 };
 
 export const registerPeer = async (peer) => {
