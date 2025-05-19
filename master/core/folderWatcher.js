2e0966fde40aeed5a4c4477913576e2164d19451
priyabahuguna <priyabahuguna220@gmail.com>
2025-05-19 23:44:40 +0530

    add file watcher

--- master/core/folderWatcher.js
+++ master/core/folderWatcher.js
@@ -1,11 +1,19 @@
 import chokidar from 'chokidar';
+import path from 'path';
 import config from '../config/appConfig.js';
 import { handleNewFile } from '../services/fileService.js';
 
-export const startFolderWatcher = () => {
+export const startWatcher = () => {
   const watcher = chokidar.watch(config.paths.userUploads, {
     ignored: /(^|[/\\])\../,
     persistent: true,
+    ignoreInitial: true,
   });
-  watcher.on('add', (filePath) => handleNewFile(filePath));
+  watcher.on('add', (filePath) => {
+    const fileName = path.basename(filePath);
+    console.log(`[Watcher] New file detected: ${fileName}`);  
+    setTimeout(() => {
+      handleNewFile(filePath, fileName);
+    }, 500);
+  });
 };
