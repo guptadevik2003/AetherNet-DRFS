6fc229c14de2f56a9723975b020e1f20358d1fe2
priyabahuguna <priyabahuguna220@gmail.com>
2025-05-26 11:08:52 +0530

    Modified Services

--- master/services/fileService.js
+++ master/services/fileService.js
@@ -1,64 +1,40 @@
-import fs from 'fs';
-import path from 'path';
-import crypto from 'crypto';
 import config from '../config/appConfig.js';
 import { chunkFile } from '../utils/chunker.js';
 import { generateParityChunks } from '../utils/reedSolomonCodec.js';
 import { encryptAndSaveChunks } from '../utils/encryption.js';
-
-import { timeCalc } from '../utils/timeCalc.js';
-import { addFileMetadata } from '../core/dhtManager.js';
+import { distributeChunks } from './peerCommunicator.js';
+import { deleteEncryptedChunks } from '../utils/deleteChunks.js';
+import { addMetadataToDHT } from '../core/dhtManager.js';
 
 export const handleNewFile = async (filePath, fileName) => {
   try {
 
-    let totStart = Date.now();
-
-    const fileId = crypto.randomUUID();
-
-    let start = Date.now();
     // Splitting File into Data Chunks (unencrypted)
     const dataChunks = await chunkFile(filePath, config.reedSolomon.chunkCount);
-    console.log(dataChunks)
-    let end = Date.now();
-    timeCalc(end-start, 'Data Chunking');
 
-    start = Date.now();
     // Generating Parity Chunks (unencrypted)
     const parityChunks = await generateParityChunks(
       dataChunks, dataChunks.length,
       Number(config.reedSolomon.parityCount)
     );
-    console.log(parityChunks)
-    end = Date.now();
-    timeCalc(end-start, 'Generating Parity');
 
-    start = Date.now();
     // Encrypting Chunks
     const chunkMap = await encryptAndSaveChunks(
       [...dataChunks, ...parityChunks],
       config.reedSolomon.chunkCount,
       config.paths.encryptedData
     );
-    end = Date.now();
-    timeCalc(end-start, 'Encrypting Chunks');
 
-    
-    
-    
-    
-    let totEnd = Date.now();
-    timeCalc(totEnd-totStart, 'Total Time');
-    
-    console.log(chunkMap);
-
-    // Distributing File
+    // Distributing Chunks
+    const distributedChunkMap = await distributeChunks(chunkMap);
+    await deleteEncryptedChunks(chunkMap);
 
     // Updating DHT
+    await addMetadataToDHT(fileName, distributedChunkMap, dataChunks[0].length);
 
-    console.log(`[FileService] Processed file ${fileName} successfully`);
+    console.log(`[FileSvc] Processed file ${fileName} successfully`);
 
   } catch(err) {
-    console.log(`[FileService] Error processing file ${fileName}:`, err);
+    console.log(`[FileSvc] Error processing file ${fileName}:`, err);
   }
 };
