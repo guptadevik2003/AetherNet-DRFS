7c6a951092a69295166a07e9ec4e3d589f0cf006
priyabahuguna <priyabahuguna220@gmail.com>
2025-05-27 10:41:49 +0530

    Updated Folder Watcher

--- master/core/folderWatcher.js
+++ master/core/folderWatcher.js
@@ -1,19 +1,42 @@
 import chokidar from 'chokidar';
 import path from 'path';
 import config from '../config/appConfig.js';
-import { handleNewFile } from '../services/fileService.js';
+import { handleNewFile, recoverFileIfCorrupted } from '../services/fileService.js';
 
 export const startWatcher = () => {
   const watcher = chokidar.watch(config.paths.userUploads, {
     ignored: /(^|[/\\])\../,
     persistent: true,
     ignoreInitial: true,
+    awaitWriteFinish: {
+      stabilityThreshold: 1000,
+      pollInterval: 100,
+    },
   });
+
+  const recovering = new Set(); // to prevent recovery loops
+
   watcher.on('add', (filePath) => {
     const fileName = path.basename(filePath);
-    console.log(`[Watcher] New file detected: ${fileName}`);  
+    console.log(`\n[Watcher] New file detected: ${fileName}`);  
     setTimeout(() => {
       handleNewFile(filePath, fileName);
     }, 500);
+  });
+
+  watcher.on('change', (filePath) => {
+    const fileName = path.basename(filePath);
+
+    if(recovering.has(fileName)) {
+      recovering.delete(fileName);
+      return;
+    }
+
+    
+    setTimeout(() => {
+      console.log(`\n[Watcher] File change detected: ${fileName}`);
+      recovering.add(fileName);
+      recoverFileIfCorrupted(filePath, fileName);
+    }, 2000);
   });
 };
--- master/services/fileService.js
+++ master/services/fileService.js
@@ -1,10 +1,12 @@
+import fs from 'fs';
 import config from '../config/appConfig.js';
-import { chunkFile } from '../utils/chunker.js';
+import { chunkFile, chunkFileBySize } from '../utils/chunker.js';
 import { generateParityChunks } from '../utils/reedSolomonCodec.js';
-import { encryptAndSaveChunks } from '../utils/encryption.js';
+import { encryptAndSaveChunks, encryptAndCompareChunkHashes } from '../utils/encryption.js';
 import { distributeChunks } from './peerCommunicator.js';
 import { deleteEncryptedChunks } from '../utils/deleteChunks.js';
-import { addMetadataToDHT } from '../core/dhtManager.js';
+import { addMetadataToDHT, getFileDHTData } from '../core/dhtManager.js';
+import { recoverCorruptedChunks } from '../utils/recovery.js';
 
 export const handleNewFile = async (filePath, fileName) => {
   try {
@@ -35,6 +37,43 @@ export const handleNewFile = async (filePath, fileName) => {
     console.log(`[FileSvc] Processed file ${fileName} successfully`);
 
   } catch(err) {
-    console.log(`[FileSvc] Error processing file ${fileName}:`, err);
+    console.log(`[FileSvc] Error processing file ${fileName}:`, err.message);
+  }
+};
+
+export const recoverFileIfCorrupted = async (filePath, fileName) => {
+  try {
+
+    // Getting DHT Data for this file
+    const dhtData = await getFileDHTData(fileName);
+    if(!dhtData) {
+      throw new Error('File not found in DHT Mapping.');
+    }
+
+    // Splitting File into Data Chunks (unencrypted)
+    const dataChunks = await chunkFileBySize(filePath, dhtData.chunkSize, dhtData.dataChunkCount);
+
+    // Re-encrypt chunks with stored IV and comparing hashes to detect corruption
+    const corruptedIndices = await encryptAndCompareChunkHashes(dataChunks, dhtData.chunkMap);
+
+    if(corruptedIndices.length === 0) {
+      return console.log(`[FileSvc] No corruption detected for ${fileName}`);
+    }
+
+    console.log(`[FileSvc] Corruption detected at chunk(s): ${corruptedIndices.join(', ')}`);
+    
+    // Repairing corrupted chunks
+    const fixedChunks = await recoverCorruptedChunks(dataChunks, corruptedIndices, dhtData);
+
+    // Reassemble fixed chunks into a single buffer
+    const fixedBuffer = Buffer.concat(fixedChunks);
+
+    // Saving recovered file
+    await fs.writeFileSync(filePath, fixedBuffer);
+
+    console.log(`[FileSvc] File repaired and saved: ${fileName}`);
+
+  } catch(err) {
+    console.log(`[FileSvc] Error recovering file ${fileName}:`, err.message);
   }
 };
